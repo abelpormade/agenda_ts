a2cf00a495f985fa0790f02d1092403169fbd9d4
abelpormade <abel@pormade.com.br>
2025-11-25 12:40:36 -0300

    documentação de swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+
+
+
+
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API ')
+
+    .setDescription('Documentação da API')
+
+    .setVersion('1.0')
+
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
 
   app.useGlobalPipes(
     new ValidationPipe({
